e72a98eb290a1cfae7e1e4bead2a3b36d4263c25
Varun Sharma <varun_sharma@live.co.uk>
2022-09-29 21:10:38 -0700

    feat mint nft func

--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -2,4 +2,28 @@ require("dotenv").config();
 const API_URL = process.env.API_URL;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const alchemyWeb3 = createAlchemyWeb3(API_URL);
-const contract = require("../artifacts/contracts/OsunRiverNFT.sol/TorNFT.json");
+const contract = require("../artifacts/contracts/BabyNFT.sol/BabyNFT.json");
+
+const contractAddress = "0x7155A74e2CC891808175B1e32E75359fcFd95C0f";
+const nftContract = new alchemyWeb3.eth.Contract(contract.abi, contractAddress);
+
+const METAMASK_PUBLIC_KEY = process.env.METAMASK_PUBLIC_KEY;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+
+async function mintNFT(tokenURI) {
+  // get the nonce - nonce is needed for security reasons. It keeps track of the number of
+  // transactions sent from our address and prevents replay attacks.
+  const nonce = await alchemyWeb3.eth.getTransactionCount(
+    METAMASK_PUBLIC_KEY,
+    "latest"
+  );
+  const tx = {
+    from: METAMASK_PUBLIC_KEY, // our MetaMask public key
+    to: contractAddress, // the smart contract address we want to interact with
+    nonce: nonce, // nonce with the no of transactions from our account
+    gas: 1000000, // fee estimate to complete the transaction
+    data: nftContract.methods
+      .createNFT("0x7155A74e2CC891808175B1e32E75359fcFd95C0f", tokenURI)
+      .encodeABI(), // call the createNFT function from our OsunRiverNFT.sol file and pass the account that should receive the minted NFT.
+  };
+}
